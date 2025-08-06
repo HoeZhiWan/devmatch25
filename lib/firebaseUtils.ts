abc05f77c628736069b817836148731ebbc06073
PS0514 <phoebesurin@gmail.com>
2025-08-06 23:45:30 +0800

    merging

--- lib/firebaseUtils.ts
+++ lib/firebaseUtils.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhfsOSLGNk5S-SXYxvlTIFyLiJ1i3iP9k",
-    authDomain: "apu-watnameleh.firebaseapp.com",
-    projectId: "apu-watnameleh",
-    storageBucket: "apu-watnameleh.firebasestorage.app",
-    messagingSenderId: "966069106843",
-    appId: "1:966069106843:web:92dfe8702b8e079c53ee3e",
-    measurementId: "G-K19JQ6ZBRJ"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
